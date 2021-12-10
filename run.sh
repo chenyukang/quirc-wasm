0cb6f42ecb83c3390d9bf1adcde85f498bf58a51
chenyukang <moorekang@gmail.com>
2021-12-10 16:06:09 +0800

    add options

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ emcc -c decode.c -o decode.o
 emcc -c identify.c -o identify.o
 emcc -c quirc.c -o quirc.o
 emcc -c version_db.c -o version_db.o
-emcc decode.o identify.o version_db.o quirc.o qrtest.o dbgutil.o -o qrtest.js  -lm  -s USE_LIBJPEG -s USE_LIBPNG
+emcc -g -Oz --llvm-lto 1  -s STANDALONE_WASM decode.o identify.o version_db.o quirc.o qrtest.o dbgutil.o -o qrtest.wasm  -lm  -s USE_LIBJPEG -s USE_LIBPNG
